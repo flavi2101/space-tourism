7861cc1ed5356351a586a229d910b307c79ad542
Flavio Henrique Ferreira <flavi2101@gmail.com>
2022-07-13 19:00:38 -0300

    style home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,21 @@
-import style from './Home.module.css'
+import style from "./Home.module.css";
 
 export default function Home() {
   return (
     <main className={style.container}>
-      <h1>home</h1>
+      <hgroup>
+        <h2>So, you want to travel to</h2>
+        <h1>Space</h1>
+      </hgroup>
+      <p>
+        Let’s face it; if you want to go to space, you might as well genuinely
+        go to outer space and not hover kind of on the edge of it. Well sit
+        back, and relax because we’ll give you a truly out of this world
+        experience!
+      </p>
+      <div>
+        <span>Explore</span>
+      </div>
     </main>
   );
 }
